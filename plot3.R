aed00291661515fd23eb1a5b180d6308080f4b72
chuckswan <39227557+chuckswan@users.noreply.github.com>
2018-07-07 16:25:04 -0500

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -23,7 +23,7 @@ plot(Sub_metering_1 ~ datetime, type = "l",
 lines(Sub_metering_2 ~ datetime, col = "Red")
 lines(Sub_metering_3 ~ datetime, col = "Blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+       legend = c("Sub_metering_1 ", "Sub_metering_2 ", "Sub_metering_3 "))
 dev.copy(png, file = "plot3.png", height = 480, width = 480)
 dev.off()
 detach(data)
